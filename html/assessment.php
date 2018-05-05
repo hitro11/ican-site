f5adeffb72d2f1b0cdbf67bca4d91170d3f9c37c
rohit <nazarera@mcmaster.ca>
2018-05-05 10:07:54 -0400

    1st section of homepage and started on assessment page

---
+++ html/assessment.php
@@ -0,0 +1,9 @@
+
+<?php echo file_get_contents("./header.html"); ?>
+
+<h1>free assessment</h1>
+
+
+
+
+<?php echo file_get_contents("./footer.html"); ?>
---
+++ html/index.php
@@ -0,0 +1,16 @@
+
+<?php echo file_get_contents("./header.html"); ?>
+
+
+  <figure>
+    <img class="cover" src="../assets/home_bg.jpg" alt="">
+    
+  </figure>
+
+  <a href="./assessment.php" class="button btn btn-primary">Take the free assessment</a>
+  
+  
+
+
+<?php echo file_get_contents("./footer.html"); ?>
+  
